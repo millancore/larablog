909af1d741e3e84d19ee151bf7de2688f622360d
Juan Millan <juanes14569@hotmail.com>
2020-06-25 13:28:21 -0300

    Add Post Repository

---
+++ app/Repository/PostRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Post;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class PostRepository
+{
+
+    /**
+     * Create new Post
+     *
+     * @param array $data
+     * @param uuid $userId
+     * @return void
+     */
+    public function create(array $data, $userId)
+    {
+        $post = new Post;
+
+        $post->title = $data['title'];
+        $post->description = $data['description'];
+        $post->slug = Str::slug($data['title'], '-');
+        $post->user_id = $userId;
+        $post->publication_date = !isset($data['publication_date']) ? Carbon::now() : $data['publication_date'];
+
+        return $post->save();
+    }
+
+    /**
+     * Get user Post
+     *
+     * @param uuid $userId
+     * @return void
+     */
+    public function allByUser($userId)
+    {
+        return Post::where('user_id', $userId)
+                    ->orderBy('publication_date', 'desc')
+                    ->paginate(5);
+    }
+
+    /**
+     * Get Post by Slug
+     *
+     * @param string $slug
+     * @return void
+     */
+    public function getBySlug(string $slug)
+    {
+        return Post::where('slug', $slug)->first();
+    }
+
+    /**
+     * All Post
+     * 
+     * @param string $publicationOrder
+     * @return void
+     */
+    public function getAll(string $publicationOrder = 'desc')
+    {
+        return Post::orderBy('publication_date', $publicationOrder)
+                    ->paginate(6);
+    }
+}
