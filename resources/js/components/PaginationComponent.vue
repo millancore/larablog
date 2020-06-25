c6886310846590e56f5f48ae1b5f033f81860c75
Juan Millan <juanes14569@hotmail.com>
2020-06-25 18:10:17 -0300

    Create view Post Components

---
+++ resources/js/components/PaginationComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="float-right">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <article @click="show" class="pointer mb-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="card-title">
+          <h4>
+            <slot name="title"></slot>
+          </h4>
+        </div>
+        <section>
+            <slot></slot>
+        </section>
+        <small class="float-right">
+            <slot name="published"></slot>
+        </small>
+      </div>
+    </div>
+  </article>
+</template>
+
+
+<script>
+export default {
+    props: {
+        slug: String
+    },
+    methods: {
+        show() {
+            window.location.href = "/posts/" + this.slug
+        }
+    }
+
+};
+</script>
+
+
+<style scoped>
+   .pointer {
+       cursor: pointer;
+   }
+</style>
